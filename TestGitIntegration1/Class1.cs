9f64018dc9fbd4e75933f9049e0e016cb419d95b
jkljlk <kljlk@gmail.com>
2015-10-27 13:33:48 +0100

    uyuiyui

---
+++ TestGitIntegration1/Class1.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by Ranorex
+ * User: admin
+ * Date: 27.10.2015
+ * Time: 13:33
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace TestGitIntegration1
+{
+	/// <summary>
+	/// Description of Class1.
+	/// </summary>
+	public class Class1
+	{
+		public Class1()
+		{
+		}
+	}
+}
